6842be4ba2590aa4b98738a6cebb124306d7036f
yashaswi1306 <yashaswi130607@gmail.com>
2026-04-13 04:20:37 +0530

    q4

---
+++ q4/q4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h> 
+
+int main() {
+
+    int a,b;
+    void* lib=NULL;
+    char str[10];
+    char end[6]="";
+    while(scanf("%s %d %d",str,&a,&b)==3){
+
+        if(lib)
+        {
+            dlclose(lib);
+            lib=NULL;
+        }
+
+        char name[30];
+        strcpy(name,"./lib");
+        strcat(name,str);
+        strcat(name,".so");
+        // so its [name]=./lib[str].so
+        lib=dlopen(name,RTLD_LAZY);
+        typedef int (*func)(int,int); //function definition
+        func f1=(func)(dlsym(lib,str));
+        printf("%d\n",f1(a,b));
+    }
+    if (lib) dlclose(lib);
+    return 0;
+}
